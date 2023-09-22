75b28492079250ad3cf63b5307746ce4080e488b
Kalpeshiitp <kalpeshkumar267@gmail.com>
2023-09-22 17:24:28 +0530

    added body-parser

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const http = require('http');
-
-const express = require('express')
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use('/',(req,res,next)=>{
-console.log("<h1> i am alway run</h1>")
-next()
-})
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product',(req,res,next)=>{
-    console.log('<h1>Product page</h1>')
-   res.send('product page')
-})
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/products" method="POST"> <input type="text" name="title"><input type="number" name="quantity"><button type="sumbit">Add product<button/></form>'
+  );
+});
 
+app.post("/products", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from the express js</h1>");
+});
 
 const server = http.createServer(app);
 
