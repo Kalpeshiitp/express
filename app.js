7a741ff730a6bc460e48b24ad5477d8701fdcb47
Kalpeshiitp <kalpeshkumar267@gmail.com>
2023-09-23 01:32:32 +0530

    serveing HTML pages

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const shopRouter = require("./routes/shop");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/admin',adminRouter);
-app.use('/shop',shopRouter);
+app.use('/',shopRouter);
 app.use((req, res, next) => {
   res.status(404).send("<h1>Page not found</h1>");
 });
